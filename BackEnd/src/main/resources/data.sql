cec576803d1e9b3ee3ad560e3b9cde5bd77d0172
Kobe Friswell <s3783258@student.rmit.edu.au>
2020-08-03 21:34:16 +1000

    Modified default data and schema

--- BackEnd/src/main/resources/data.sql
+++ BackEnd/src/main/resources/data.sql
@@ -1,5 +1,17 @@
-INSERT into Bookings (custName, empID, date) VALUES
-    ('TestCustomer1', '3256', '2020-08-03'),
-    ('TestCustomer1', '2352', '2020-08-07'),
-    ('TestCustomer1', '2352', '2020-08-08'),
-    ('TestCustomer1', '3256', '2020-08-20');
+INSERT into Customers (custID, firstName, lastName, email) VALUES
+    ('1234', 'Ian', 'Hanson', 'ian.hanson@email.com'),
+    ('1235', 'Leila', 'Rankin', 'leila.rankin@email.com'),
+    ('1236', 'Max', 'Foreman', 'max.foreman@email.com'),
+    ('1237', 'Jessie', 'Holmes', 'jessie@holmes@email.com');
+
+INSERT into Employees (empID, firstName, lastName) VALUES
+    ('9923', 'Marisa', 'Chadwick'),
+    ('9924', 'Rick', 'Fenton'),
+    ('9925', 'Rylee', 'Wright'),
+    ('9926', 'Katelyn', 'Clarkson');
+
+INSERT into Bookings (custID, empID, date) VALUES
+    ('1234', '9923', '2020-08-03'),
+    ('1235', '9924', '2020-08-07'),
+    ('1236', '9925', '2020-08-08'),
+    ('1237', '9926', '2020-08-20');
--- BackEnd/src/main/resources/schema.sql
+++ BackEnd/src/main/resources/schema.sql
@@ -1,8 +1,25 @@
 DROP table if exists Bookings;
+DROP table if exists Customers;
+Drop table if exists Employee;
+
+CREATE table Customers(
+    custID INT PRIMARY KEY,
+    firstName VARCHAR(30) not NULL,
+    lastName VARCHAR(30) not NULL,
+    email VARCHAR(250) not NULL
+);
+
+CREATE table Employees(
+    empID INT PRIMARY KEY,
+    firstName VARCHAR(30),
+    lastName VARCHAR(30)
+);
 
 CREATE table Bookings(
     bookingID INT auto_increment PRIMARY KEY,
-    custName VARCHAR(250) not NULL,
-    empID VARCHAR(250) not NULL,
-    date DATE not NULL
+    custID INT not NULL,
+    empID INT not NULL,
+    date DATE not NULL,
+    FOREIGN KEY (custID) REFERENCES Customers(custID),
+    FOREIGN KEY (empID) REFERENCES Employees(empID)
 );
