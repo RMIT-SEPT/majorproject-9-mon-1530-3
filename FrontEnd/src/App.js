534902c7573f35b259abf41cb8af5f00841a331f
Connor Hutchinson <s3544152@student.rmit.edu.au>
2020-08-31 13:58:17 +1000

    Add Booking, navbar, booking page
    Added improvements across frontEnd and backEnd. Able to submit bookings (without error checking) from front to back via booking page. Requires UserID for now

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -2,26 +2,31 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap-theme.min.css';
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import './App.css';
+import {Provider} from "react-redux";
+import store from "./store";
+
+
 import Login from './components/loginPage';
 import Register from './components/signUpPage';
 import Index from './components/indexPage';
 import CreateBooking from './components/CreateBooking';
+import Nav from './components/Layout/Nav';
 
 function App() {
   return (
+    <Provider store = {store}>
       <Router>
         <div>
-          <header className="App-header"> Welcome
+        <Nav></Nav>
+          <header className="App-header">
               <Route exact path="/index" component={Index}/>
               <Route exact path="/login" component={Login}/>
               <Route exact path="/register" component={Register}/>
               <Route exact path="/CreateBooking" component={CreateBooking}/>
-              <a href = "/login">Login</a>
-              <a href = "/Register">Register</a>
-              <a href = "/CreateBooking">Create Booking</a>
           </header>
           </div>
       </Router>
+    </Provider>
   );
 }
 
--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -5,6 +5,6 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 
-ReactDOM.render(<App/>, document.getElementById("root"));
+ReactDOM.render(<React.StrictMode><App/></React.StrictMode>, document.getElementById("root"));
 
 serviceWorker.unregister();
