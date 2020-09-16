fccc22979a9dc8b27c5551ae598e283576782ca7
Aren Carter <s3728738>
2020-09-16 19:56:07 +1000

    nav bar done

--- FrontEnd/src/components/Layout/Nav.js
+++ FrontEnd/src/components/Layout/Nav.js
@@ -3,21 +3,11 @@ import React, { Component } from 'react'
 class Nav extends Component {
     render() {
         return (
-            <nav className = "navbar navbar-expand-sm">
-                <div className = "container">
-                    <a className = "navbar-brand" href= "/index"> Home </a>
-                    <div className = "navbar-brand">
-                            <a className = "nav-link" href = "/CreateBooking"> Create Booking </a>
-                            <a className = "nav-link" href = "/deleteBooking"> Edit/Cancel Booking </a>
-                    </div>
-                    <div className = "navbar-right">
-                        <div>
-                            <a className = "nav-link" href = "/login">Login</a>
-                        </div>
-                        <div>
-                            <a className = "nav-link" href = "/Register">Register</a>
-                        </div>
-                    </div>
+            <nav>
+                <div className = "navLinks">
+                    <a href = "/index">home</a>
+                    <a href = "/AboutUs">about us</a>
+                    <a href = "/CreateBooking">book</a>
                 </div>
             </nav>
         )
--- FrontEnd/src/components/indexPage.jsx
+++ FrontEnd/src/components/indexPage.jsx
@@ -1,13 +1,86 @@
 import React, { Component } from "react";
+import {userLogin} from "../actions/LoginActions";
 
 class Index extends Component {
   state = {};
+  //CONSTRUCTS A UN/PW/ERROR
+  constructor() {
+    super();
+
+    this.state = {
+        username: "",
+        password: "",
+        errorMessage: ""
+    };
+
+    this.state = {isToggleOn: true};
+
+    // this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  toggle(event) {
+    var signin = document.getElementById("signinBox");
+    var signup = document.getElementById("signupBox");
+
+    if(signin.style.display === "block") {
+      signup.style.display = "none";
+      signin.style.display = "block";
+    } else if (signup.style.display === "block") {
+      signin.style.display = "none";
+      signup.style.display = "block";
+    }
+    
+  }
+
+  async onSubmit(event){
+        event.preventDefault();
+        const user = {
+            "username":this.state.username,
+            "password":this.state.password,
+            "userType":"Customer"
+        }
+        console.log(user)
+        let valid = await userLogin(user)
+
+        if (valid){
+            console.log("User Found :)")
+        } else{
+            console.log("User Not Found!")
+        }
+    }
+
+
+  //THIS RENDER() DISPLAYS ON THE PAGE
   render() {
     return (
+      
       <div>
-        <h1>Index</h1>
+      <div className="signs" id="signinBox">
+        <form onSubmit={this.onSubmit}>
+        <input className="input" name="username"
+           placeholder="username"
+           value={this.state.username}
+           onChange={this.onChange}
+        />
+          <br></br>
+        <input className="input" name="password"
+           placeholder="password"
+            type="password"
+            value={this.state.password}
+            onChange={this.onChange}
+        />
         <br></br>
+        <input className="login" type="submit" value="login." onSubmit={this.onSubmit}/>
+        <button id="signupButton" onClick={this.toggle}>sign up instead?</button>
+        </form>
       </div>
+
+      <div className="signs" id="signupBox">
+        <p>fat</p>
+      </div>
+      </div>
+
     );
   }
 }
