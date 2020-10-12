1e863da61de484a39a5d206c3704417b1b9df6a6
Aren Carter <50359993+s3728738@users.noreply.github.com>
2020-10-12 15:52:50 +1100

    work in progress for booking history

--- FrontEnd/src/components/CustomerBookingHistory.jsx
+++ FrontEnd/src/components/CustomerBookingHistory.jsx
@@ -1,11 +1,50 @@
 import React, { Component } from "react";
+import {getAllBookings} from "../actions/bookingActions";
 
 class CustomerBookingHistory extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isLoading: true,
+            bookings: [],
+            filterBookings: []
+        }
+    }
+
+    componentDidMount() {
+
+        var filtered = [];
+
+        this.state.bookings.forEach((booking) => {
+            if(booking.customerID === sessionStorage.getItem("username")) {
+                filtered.push(booking)
+            }
+        })
+
+        this.setState({filterBookings:filtered});
+
+    }
+
+    fetchData() {
+
+    }
+
+
     render() {
+
+        let list = this.state.bookings.map((booking) => {
+            return <li>"Date of Booking: " {booking.date}</li>
+        });
+
+        let beees = <p>bee</p>
+        
         return(
             <div>
                 <p>LIST OF ALL BOOKINGS</p>
+                {list}
+                {beees}
             </div>
         )
     }
